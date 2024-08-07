9b023b4a9cea6ae901a9a0f6c45d6dffbd4f85c1
Sarah Eja <ejasarah1@gmail.com>
2024-08-07 15:22:39 +0100

    a todo app

---
+++ src/App.tsx
@@ -0,0 +1,67 @@
+import styled from "styled-components";
+import TodoList from "./pages/todoList/TodoList";
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: lightgray;
+
+  .todoList {
+    background-color: skyblue;
+    padding: 20px;
+    border-radius: 8px;
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+  }
+
+  .todoListHeader {
+    display: flex;
+    gap: 12px;
+
+    input {
+      padding: 12px;
+      border-radius: 8px;
+      border-color: 1px solid #eaeaea;
+      width: 300px;
+    }
+
+    button {
+      padding: 12px;
+      border-radius: 8px;
+      border: 1px solid #eaeaea;
+      background-color: skyblue;
+    }
+  }
+
+  .todoListBody {
+    .todoItem {
+      display: flex;
+      justify-content: space-between;
+      border: 1px solid green;
+      padding: 10px;
+      align-items: center;
+      border-radius: 8px;
+
+      .title {
+        font-size: 14px;
+        font-weight: 500;
+      }
+    }
+  }
+`;
+
+const App = () => {
+  return (
+    <>
+      <Wrapper>
+        <TodoList />
+      </Wrapper>
+    </>
+  );
+};
+
+export default App;
